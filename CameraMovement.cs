a776565c3ecb241ede77d42aab8ebf3995886820
Rafael Kenzo Mippo <rkmippo@gmail.com>
2021-10-05 16:24:23 -0300

    Fixed Camera movement

--- CameraMovement.cs
+++ CameraMovement.cs
@@ -5,40 +5,23 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
 
-    [SerializeField] private float fOffset = 1.5f;
+    [SerializeField] private float fSmooth;
+    [SerializeField] private Vector3 offset;
     private GameObject Player;
-    private float xPosition;
-    private float yPosition;
-    private bool isMoving;
+    private Vector3 actualPosition;
+    private Vector3 targetPosition;
     
     private void Start()
     {
         Player = GameObject.FindGameObjectWithTag("Player");
-        isMoving = false;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         //follow the player
-        xPosition = Player.transform.position.x;
-        this.transform.position = new Vector3(xPosition, Player.transform.position.y + 5, -10);
-
-        /*awfull camera. Need rework
-        if (transform.position.y < Player.transform.position.y - fOffset)
-        {
-            isMoving = true;
-        }
-        else if (transform.position.y > Player.transform.position.y + fOffset) 
-        {
-            yPosition = transform.position.y - 20 * Time.deltaTime;
-            isMoving = false;
-        }
-        if (isMoving) 
-        {
-            yPosition = transform.position.y + 40 * Time.deltaTime;
-        }
-        this.transform.position = new Vector3(xPosition, yPosition, -10);
-        */
+        targetPosition = Player.transform.position + offset;
+        actualPosition = Vector3.Lerp(transform.position, targetPosition, fSmooth);
+        transform.position = actualPosition;
     }
 }
