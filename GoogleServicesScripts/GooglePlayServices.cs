2d5daae8d812eb5e685a11c4a87732eed1a0bef9
Rafael Kenzo Mippo <rkmippo@gmail.com>
2021-10-09 17:50:05 -0300

    Add Google Play Services integration

---
+++ GoogleServicesScripts/GooglePlayServices.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+
+public class GooglePlayServices : MonoBehaviour
+{
+
+    private bool isConnectedToGoogle = false;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        PlayGamesPlatform.DebugLogEnabled = true;
+        PlayGamesPlatform.Activate();
+    }
+
+    private void Start()
+    {
+        // log in
+        PlayGamesPlatform.Instance.Authenticate(GooglePlayGames.BasicApi.SignInInteractivity.CanPromptOnce, (result) =>
+        {
+            if (result == SignInStatus.Success)
+            {
+                isConnectedToGoogle = true;
+            }
+            else 
+            {
+                isConnectedToGoogle = false;
+            }
+        });
+
+        // set pop-up to the bottom
+        Social.localUser.Authenticate((bool success) =>
+            {
+                if (success) 
+                {
+                    ((GooglePlayGames.PlayGamesPlatform)Social.Active).SetGravityForPopups(Gravity.BOTTOM);
+                }
+            });
+    }
+
+}
