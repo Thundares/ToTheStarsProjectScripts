d8dc60b519faa2a67f52462ced623b89df0dfd98
Rafael Kenzo Mippo <rkmippo@gmail.com>
2021-10-14 13:41:09 -0300

    Remove google play services
    Fix Height UI background

--- Controller/HeightController.cs
+++ Controller/HeightController.cs
@@ -9,6 +9,7 @@ public class HeightController : MonoBehaviour
     [SerializeField] private GameObject panel = null;
     private RectTransform panelRect;
     float panelWidth;
+    int mult;
 
     private void Start() 
     {
@@ -19,7 +20,9 @@ public class HeightController : MonoBehaviour
     {
         VariablesManager.dHeight = player.position.y / 10;
         numberText.text = "H: " + VariablesManager.dHeight.ToString("N0") + " M";
-        panelWidth = (float)Math.Log10(VariablesManager.dHeight) * 55 + 375;
+        mult = (int)Math.Floor(Math.Log10(VariablesManager.dHeight));
+        mult = mult < 1 ? 1 : mult;
+        panelWidth = (float)mult * 55 + 275;
         if (float.IsNaN(panelWidth)) 
         {
             panelWidth = 375;
