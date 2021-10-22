5d5ccc5207c291346f3eb7a64cd5f81c2708c41f
Rafael Kenzo Mippo <rkmippo@gmail.com>
2021-10-22 15:10:04 -0300

    Fix Volume script to user friendly values

--- UI/Volume.cs
+++ UI/Volume.cs
@@ -12,15 +12,41 @@ public class Volume : MonoBehaviour
     {
         if (master)
         {
-            am.SetFloat("volume", volume);
+            am.SetFloat("volume", TranslatorValues(volume));
         }
         else if (music)
         {
-            am.SetFloat("musicVolume", volume);
+            am.SetFloat("musicVolume", TranslatorValues(volume));
         }
         else if (SE) 
         {
-            am.SetFloat("seVolume", volume);
+            am.SetFloat("seVolume", TranslatorValues(volume));
         }
+    }
+
+    private float TranslatorValues(float value05) 
+    {
+        if (value05 < 0.5f) 
+        {
+            return -80f;
+        }
+        if (value05 < 1.5f) 
+        {
+            return -40f;
+        }
+        if (value05 < 2.5f) 
+        {
+            return -20f;
+        }
+        if (value05 < 3.5f) 
+        {
+            return -10f;
+        }
+        if (value05 < 4.5f) 
+        {
+            return -5f;
+        }
+        // then it is 5
+        return 0;
     }
 }
